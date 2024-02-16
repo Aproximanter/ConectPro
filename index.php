518f9e635ede6b0e407ce30764a387ddaf467a3d
Aproximanter <148830600+Aproximanter@users.noreply.github.com>
2024-02-15 21:10:02 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            margin: 0;
+            padding: 0;
+        }
+
+    
+    </style>
+</head>
+<body class = "bg-light">
+    <div class="container-fluid bg-primary text-white text-center py-5">
+        <h1 class="display-1 text-center">Bienvenido</h1>
+        
+    </div>
+<div class = "container px-4">
+  <div class="row gx-5">
+<p class="lead border bg-light"><small> ¡Te damos la bienvenida a ConectaPro, el lugar donde las conexiones 
+  profesionales cobran vida! En un mundo laboral en constante evolución, 
+  la necesidad de conectarse de manera efectiva con profesionales competentes es 
+  esencial. ConectaPro ha sido creado con la visión de simplificar y mejorar la 
+  forma en que profesionales y usuarios se encuentran, colaboran y prosperan. </small></p>
+
+</div>
+
+</div>
+<div class = "container px-4">
+  <div class="row gx-5">
+  <img src="https://cdn.milenio.com/uploads/media/2017/07/21/profesionistas-empleados-meses-egreso-estudiaron.jpeg" class="img-fluid" alt="...">
+</div>
+    <div class="container-fluid text-center">
+        <button type="button" id="entrar" class="btn btn-primary btn-lg">Iniciar sesión</button>
+    </div>
+    <script>
+     document.getElementById('entrar').addEventListener('click', function() {
+    window.open('https://www.youtube.com', '_blank');
+});
+    </script>
+
+    
+</body>
+</html>
